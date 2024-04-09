3ab8303db50fb5d6b37d84197794832a906148e5
Apilriddhasoft <apil.lamsal@riddhasoft.com>
2024-04-09 09:21:57 +0545

    Second update Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
-import Navbar from './Components/Navbar/Navbar'
-
+import React from 'react';
+import Navbar from './Components/Navbar/Navbar';
+import Hero from './Components/Hero/Hero';
+import "./index.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Products from './Components/Products/Products'
+import AOS from "aos";
+import "aos/dist/aos.css";
 const App = () => {
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
+
   return (
     <div>
-      <Navbar/>
-    
+      <Navbar />
+      <Hero />
+      <Products />
+      
     </div>
   )
 }
